c89e91ab0be24d626596b2175f48064f22f09f3d
saamuel <saamuel.auvaart@voco.ee>
2024-10-25 09:27:10 +0300

    fisnished project

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require("express")
 const app = express()
-
 const path = require("path")
+const fetch = require("node-fetch")
 
 app.set("view engine", "ejs")
-app.set ("views", path.join(__dirname, "views"))
+app.set("views", path.join( dirname, "views"))
 
-app.get("/", function (req,res) {
-  res.render("index")
-})
+const key = "771970598769bf8861004c74e38ebbba";
+let city = "Tartu"
+
+app.get("/", function (req, res) {
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=${    city}&appid=${key}")
+      .then((responce) => { 
+        return responce.json()
+      })
+      .then((data) => {
+        let description = data weather[0].description
+        let city = data name
+        let temp = data main temp
+        console.log(description)
+        console.log()
+        console.log(data);
+          })
+          res.render("'index")
+        })
 
 app.listen(3000)
